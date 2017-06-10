03e0802afbbbc8376855769b00d670fce56fb44e
Unknown <fabrice.bossuyt@gmail.com>
2017-06-10 19:05:28 +0200

    commit

--- auth/authentication.controller.js
+++ auth/authentication.controller.js
@@ -6,6 +6,7 @@ var Promise = require('bluebird');
 var User = require('../user/user.model.js');
 var securityConfig = require('../config/security.config.js');
 var passport = require('passport')
+var crypto  = require('crypto')
 
 const router = express.Router();
 
@@ -30,8 +31,7 @@ router.post('/register', (req, res) => {
 });
 
 router.get('/facebook',
-  passport.authenticate('facebook', { session: false, scope: ['email'] }), function (req, res) {
-  }
+  passport.authenticate('facebook', { session: false, scope: ['email'] }), function (req, res) {}
 );
 
 router.get('/facebook/callback',
@@ -43,8 +43,42 @@ router.get('/facebook/callback',
   }
 );
 
-router.get('/password-reset', function () {
-
+router.post('/reset-password', function (req, res) {
+  var user = User.forge({password_reset_token: token});
+  user.fetch().then (function(model){
+    if (moment() > model.password_reset_token_ttl)
+      res.json({success : false, msg : 'Token timed out'});
+    else 
+      res.json({success: true, msg: ''});
+  })
 });
+
+router.post('/password-reset', function (req, res) {
+  var email = req.body.email;
+  var user = User.forge({ email: email });
+
+  user.fetch().then(function (model) {
+    if (model && !model.facebook_id) {
+      var token = crypto.randomBytes(8).toString();
+      console.log(token)
+      user.save({ password_reset_token: token, password_reset_token_ttl: moment().add(1, 'hours')}).then(function(response){
+        sendMail(response.attributes)
+      })
+    }
+    else
+      res.json({ success: false, msg: 'No such user' })
+  })
+});
+
+router.post('/password-change', function(req, res){
+  var password = req.body.password; 
+
+})
+
+function sendMail(user){
+  //implement mail sending api
+  var mailContent = 'token  + text'
+  var mailTo = user.email;
+}
 
 module.exports = router;
