109fd432a6bbe20d8ebcb1eedae930f4732a830a
Unknown <fabrice.bossuyt@gmail.com>
2018-05-02 23:13:52 +0200

    wip

---
+++ ingredient/ingredient.migration.js
@@ -0,0 +1,21 @@
+'use strict'
+
+var dbConfig = require('../config/database.config.js')
+var knex = require('knex')(dbConfig)
+
+exports.up = function() {
+  return knex.schema
+  .createTableIfNotExists('ingredients', function(table) {
+    table.increments('id').primary();
+    table.string("name").unique().notNullable();
+    table.string("code").unique().notNullable();
+    table.timestamps();
+  })
+};
+
+exports.down = function() {
+  return knex.schema
+  .dropTable('ingredients')
+};
+
+module.exports = exports;
